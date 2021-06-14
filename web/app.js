653b6fd5c5fb822f648a72aa6ee603ac2ff9a1c8
Kydo Solis <kydo.s@alphaonedesign.com>
2021-06-14 17:22:41 +0800

    Integrate frontend and backend run

--- web/app.js
+++ web/app.js
@@ -7,6 +7,7 @@ const fs = require('fs');
 const cookieParser = require('cookie-parser');
 const handleErrors = require('./middlewares/handle-errors.middleware');
 const cors = require('./middlewares/cors.middleware');
+const path = require('path');
 
 const port = process.env.PORT || 3000;
 const routePath = './routes/';
@@ -14,9 +15,11 @@ const app = express();
 
 // Settings
 app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cors);
+app.use(express.static(path.join(__dirname, './views/build')));
+app.use('/', express.static(path.join(__dirname, './views/build')));
 
 // Load Routes Dynamically
 fs.readdirSync(routePath).forEach(function (file) {
